7edd2c6e54223aef86a49c0b07aab6272c6a2744
lew2mz <david.lewis@cchmc.org>
2022-10-31 14:41:04 -0400

    fixed sha256

--- default.nix
+++ default.nix
@@ -16,7 +16,7 @@ let
     version = "22.10.0";
     src = fetchurl {
       url = "https://github.com/nextflow-io/nextflow/releases/download/v${version}/nextflow-${version}-all";
-      sha256 = "sha256-as6ovSH39msTY+75AM1pbZUj0rnttTMnlA8JMYnBU14=";
+      sha256 = "sha256-pxS+68tUTWwUAoYhk0CpRA1BLSrVemkd4PBhBoQgToM=";
     };
     dontUnpack = true;
     nativeBuildInputs = [ makeWrapper ];
